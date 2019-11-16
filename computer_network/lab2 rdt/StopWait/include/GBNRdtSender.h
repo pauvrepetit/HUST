1ab0adfd47f7078eb225ab97d1f3acee33aac4f1
pauvrepetit <hu__ao@outlook.com>
2019-11-16 18:45:04 +0800

    GBN rdt

--- computer_network/lab2 rdt/StopWait/include/GBNRdtSender.h
+++ computer_network/lab2 rdt/StopWait/include/GBNRdtSender.h
@@ -1,25 +1,23 @@
 #ifndef GBN_RDT_SENDER_H
 #define GBN_RDT_SENDER_H
-#include "RdtSender.h"
 #include "Global.h"
-class GBNRdtSender :public RdtSender
-{
-private:
-
-public:
+#include "RdtSender.h"
+class GBNRdtSender : public RdtSender {
+  private:
+  public:
     int seqNum;
     int windowBase;
     int countPackage;
     Packet sendPackage[SEQ_MAX + 1];
 
-	bool getWaitingState();
-	bool send(const Message &message);
-	void receive(const Packet &ackPkt);
-	void timeoutHandler(int seqNum);
+    bool getWaitingState();
+    bool send(const Message &message);
+    void receive(const Packet &ackPkt);
+    void timeoutHandler(int seqNum);
 
-public:
-	GBNRdtSender();
-	// virtual ~GBNRdtSender();
+  public:
+    GBNRdtSender();
+    // virtual ~GBNRdtSender();
 };
 
 #endif
